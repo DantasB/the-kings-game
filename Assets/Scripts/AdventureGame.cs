b26ff9dd79093dced968353dcf716b34d925f9ab
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-01-16 20:54:52 -0300

    [First Commit] Adding the Game

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] private Text textComponent;
+    [SerializeField] private State startingState;
+
+    private State currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState       = startingState;
+        textComponent.text = currentState.GetStateStory();
+    }
+
+    private void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = currentState.GetNextStates();
+        for(int i = 0; i < nextStates.Length; i++)
+        {
+            ChangeState(i, nextStates);
+        }        
+
+        textComponent.text = currentState.GetStateStory();
+
+        EndGame();
+
+    }
+
+    private void ChangeState(int i, State[] nextStates)
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+        {
+            currentState = nextStates[i];
+        }
+    }
+
+    private void EndGame()
+    {
+        if (currentState.GetStateStory().ToUpper().Contains("APERTE Q"))
+        {
+            QuitApplication();
+        }
+    }
+
+    private void QuitApplication()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+    }
+}
